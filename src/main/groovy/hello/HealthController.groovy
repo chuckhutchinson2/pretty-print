7c3c0dc2aaa057ff14842e3dfae0155d4d65282b
chuck hutchinson <chuckhutchinson2@yahoo.com>
2017-04-07 16:18:25 -0400

    added support for CORS

--- src/main/groovy/hello/HealthController.groovy
+++ src/main/groovy/hello/HealthController.groovy
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
 
 import groovy.json.JsonOutput
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HealthController {
 	@RequestMapping(value = "/", method=RequestMethod.GET)
 	public @ResponseBody String health() {
--- src/main/groovy/hello/PrettyPrintController.groovy
+++ src/main/groovy/hello/PrettyPrintController.groovy
@@ -5,13 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
 
 import groovy.json.JsonOutput
 
+import org.slf4j.*
+import groovy.util.logging.Slf4j
+
+@Slf4j
 @RestController
+@CrossOrigin(origins = "*")
 public class PrettyPrintController {
-	@RequestMapping(value = "/print", method=RequestMethod.POST)
+	@RequestMapping(value = "/print", method=RequestMethod.POST, produces="application/json", consumes="application/json")
 	public @ResponseBody String print(@RequestBody String data) {
+		log.info(data)
 		return JsonOutput.prettyPrint(data)
 	}
 }
